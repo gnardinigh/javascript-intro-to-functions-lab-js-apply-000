14528f1c64b4b13250428c4cc9619f23e771a293
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-04 04:29:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   }
   else if(string==string.toUppercase){
-    return
+    return "YES INDEED!";
+  }
+  else if(string=="I love you, Grandma."){
+    return "I love you, too."
   }
 }

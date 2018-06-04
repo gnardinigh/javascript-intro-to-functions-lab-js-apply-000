655dd9d188d34c29a6872a168437794e40c93d31
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-04 04:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ function shout(string){
 }
 
 function whisper(string){
-  string.toLowercase();
+  string.toLowerCase();
 }
 
 function logShout(string){
-  console.log(string.toUppercase());
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string){
-  console.log(string.toLowercase());
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string){
-  if(string==string.toLowercase()){
+  if(string==string.toLowerCase()){
     return "I can't hear you!";
   }
-  else if(string==string.toUppercase()){
+  else if(string==string.toUpperCase()){
     return "YES INDEED!";
   }
   else if(string=="I love you, Grandma."){
